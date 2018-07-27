22f5745197505513fb22b2a02364d925b61cee86
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-07-27 16:43:39 +0200

    fourth commit

--- JS2/Week5/Practice/script.js
+++ JS2/Week5/Practice/script.js
@@ -6,19 +6,18 @@ btn.addEventListener("click", function(){
     var ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', 'https://api.github.com/orgs/HackYourFuture/repos');
     ourRequest.onload = function(){
-        if(ourRequest.status >= 200 && ourRequest.status > 400) {
+        
              var ourData =JSON.parse(ourRequest.responseText);
              renderHTML(ourData);
-        }       
-        else{
+       
             console.log("We connected to the server , but it returned an error");
-            }
+           
     };
 
     ourRequest.onerror =function(){
         console.log("Connection Error");
     };
-    
+
     ourRequest.send();
 });
 
