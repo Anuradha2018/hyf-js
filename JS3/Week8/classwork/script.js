183a90fb83e5811fd606fd3569193a1d2b80add6
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-08-17 20:54:49 +0200

    commit

---
+++ JS3/Week8/classwork/script.js
@@ -0,0 +1,35 @@
+/*Exercise 1
+
+ONLY using promises! 
+a. Wait for 3 seconds, then fetch movies using this url https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json and log the result out
+
+b. When that is done, fetch the reddit programmerhumor posts. 'https://www.reddit.com/r/ProgrammerHumor.json'
+
+Use chaining instead of 
+
+*/
+
+const movieAPI = "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json";
+const redditAPI = "https://www.reddit.com/r/ProgrammerHumor.json";
+
+
+setTimeout(()=>{
+
+    fetch(movieAPI)
+        .then(response =>{
+            return response.json();
+        })
+        .then(json => {
+            console.log(json);
+            return fetch(redditAPI);
+        })
+        .then(data =>{
+            return data.json();
+            console.log(data.json)
+        })
+        .then(finalData =>{
+            console.log(finalData);
+        })
+        .catch(err => console.log(error));
+
+},3000);
